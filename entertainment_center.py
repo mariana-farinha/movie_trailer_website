aeebf9875ae97783f68fcf489fad094fd7b14587
Mary <mary.farinha@gmail.com>
2015-11-22 11:48:16 +0000

    Add Movies Website

---
+++ entertainment_center.py
@@ -0,0 +1,7 @@
+import fresh_tomatoes
+import media
+
+toy_story = media.Movie("Toy Soty", "Toys come to life", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+movies = [toy_story]
+fresh_tomatoes.open_movies_page(movies)
